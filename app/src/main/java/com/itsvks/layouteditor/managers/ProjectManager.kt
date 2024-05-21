239160e23c66543006d3c447031042db5ee461c8
Roman.Lapa <roman.lapa@expanice.com>
2024-05-21 19:44:53 +0300

    ProjectManager.kt context update

--- app/src/main/java/com/itsvks/layouteditor/managers/ProjectManager.kt
+++ app/src/main/java/com/itsvks/layouteditor/managers/ProjectManager.kt
@@ -1,5 +1,6 @@
 package com.itsvks.layouteditor.managers
 
+import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.itsvks.layouteditor.LayoutEditor
@@ -12,72 +13,74 @@ import java.util.Objects
 import java.util.concurrent.CompletableFuture
 
 class ProjectManager private constructor() {
-  private val paletteList: MutableList<List<HashMap<String, Any>>> = ArrayList()
-  var openedProject: ProjectFile? = null
-    private set
+    private lateinit var context: Context
+    private val paletteList: MutableList<List<HashMap<String, Any>>> = ArrayList()
+    var openedProject: ProjectFile? = null
+        private set
 
-  init {
-    CompletableFuture.runAsync { initPalette() }
-  }
-
-  fun openProject(project: ProjectFile?) {
-    openedProject = project
-    openedProject!!.drawables?.let { DrawableManager.loadFromFiles(it) }
-    FontManager.loadFromFiles(openedProject!!.fonts)
-  }
-
-  fun closeProject() {
-    openedProject = null
-    DrawableManager.clear()
-    FontManager.clear()
-  }
-
-  val colorsXml: String
-    get() = FileUtil.readFile(openedProject!!.colorsPath)
-  val stringsXml: String
-    get() = FileUtil.readFile(openedProject!!.stringsPath)
-  val formattedProjectName: String
-    get() {
-      var projectName = openedProject!!.name.lowercase(Locale.getDefault()).trim { it <= ' ' }
-      if (projectName.contains(" ")) {
-        projectName = projectName.replace(" ".toRegex(), "_")
-      }
-      if (!projectName.endsWith(".xml")) {
-        projectName = "$projectName.xml"
-      }
-      return projectName
+    fun initManger(context: Context) {
+        this.context = context
+        CompletableFuture.runAsync { initPalette(context) }
     }
 
-  fun getPalette(position: Int): List<HashMap<String, Any>> {
-    return paletteList[position]
-  }
+    fun openProject(project: ProjectFile?) {
+        openedProject = project
+        openedProject!!.drawables?.let { DrawableManager.loadFromFiles(it) }
+        FontManager.loadFromFiles(openedProject!!.fonts)
+    }
 
-  private fun initPalette() {
-    val gson = Gson()
-    val type = object : TypeToken<ArrayList<HashMap<String?, Any?>?>?>() {}.type
-    paletteList.clear()
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_COMMON))
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_TEXT))
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_BUTTONS))
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_WIDGETS))
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_LAYOUTS))
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_CONTAINERS))
-    // paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_GOOGLE));
-    paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_LEGACY))
-  }
+    fun closeProject() {
+        openedProject = null
+        DrawableManager.clear()
+        FontManager.clear()
+    }
 
-  private fun convertJsonToJavaObject(
-    gson: Gson, type: Type, filePath: String
-  ): ArrayList<HashMap<String, Any>> {
-    return gson.fromJson(
-      FileUtil.readFromAsset(filePath, LayoutEditor.instance!!.context), type
-    )
-  }
+    val colorsXml: String
+        get() = FileUtil.readFile(openedProject!!.colorsPath)
+    val stringsXml: String
+        get() = FileUtil.readFile(openedProject!!.stringsPath)
+    val formattedProjectName: String
+        get() {
+            var projectName = openedProject!!.name.lowercase(Locale.getDefault()).trim { it <= ' ' }
+            if (projectName.contains(" ")) {
+                projectName = projectName.replace(" ".toRegex(), "_")
+            }
+            if (!projectName.endsWith(".xml")) {
+                projectName = "$projectName.xml"
+            }
+            return projectName
+        }
 
-  companion object {
+    fun getPalette(position: Int): List<HashMap<String, Any>> {
+        return paletteList[position]
+    }
 
-    @JvmStatic
-    @get:Synchronized
-    val instance: ProjectManager by lazy { ProjectManager() }
-  }
+    private fun initPalette(context: Context) {
+        val gson = Gson()
+        val type = object : TypeToken<ArrayList<HashMap<String?, Any?>?>?>() {}.type
+        paletteList.clear()
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_COMMON, context))
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_TEXT, context))
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_BUTTONS, context))
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_WIDGETS, context))
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_LAYOUTS, context))
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_CONTAINERS, context))
+        // paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_GOOGLE));
+        paletteList.add(convertJsonToJavaObject(gson, type, Constants.PALETTE_LEGACY, context))
+    }
+
+    private fun convertJsonToJavaObject(
+        gson: Gson, type: Type, filePath: String, context: Context
+    ): ArrayList<HashMap<String, Any>> {
+        return gson.fromJson(
+            FileUtil.readFromAsset(filePath, context), type
+        )
+    }
+
+    companion object {
+
+        @JvmStatic
+        @get:Synchronized
+        val instance: ProjectManager by lazy { ProjectManager() }
+    }
 }
